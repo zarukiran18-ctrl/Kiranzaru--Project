e51cb80776f009fb15dd1370cb966fb348d752c6
Kiran Zaru <zarukiran18@gmail.com>
2025-12-12 17:52:19 -0500

    Initial Commit

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-2.cluster-c01uecugsfww.us-east-1.rds.amazonaws.com',
+    DB_USER : 'kiran',
+    DB_PWD : 'Kiranzaru$123',
+    DB_DATABASE : 'webappdb'
 });
